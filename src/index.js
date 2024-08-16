b326d4af7ca3c2282c3683770c85840c4945f2f4
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-08-16 17:21:37 +0200

    remodeling css to fit to the initial project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// self imports
+import Application from './Application';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>
 );
 
