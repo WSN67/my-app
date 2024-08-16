637dfbaa2010939a04c490ba7a38ed7ea9e4b951
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-08-16 17:21:18 +0200

    first skelleton of the screen

---
+++ src/Application.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropsType from 'prop-types';
+import './Application.css';
+
+function Application() {
+    return <>
+        <div className="Application-container">
+            <img className="Application-img" src="https://cdn.pixabay.com/photo/2024/05/24/19/06/bird-8785666_960_720.jpg"></img>
+            <footer className="Application-footer">footer</footer>
+        </div>
+    </>;
+}
+export default Application
