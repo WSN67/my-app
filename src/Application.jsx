b172644c62eaaa7360939300da731de8df626c1e
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-09-05 15:32:31 +0200

    add buttons and css to nav bar

--- src/Application.jsx
+++ src/Application.jsx
@@ -11,11 +11,26 @@ function Application() {
         while (container.firstChild) {
             container.removeChild(container.firstChild);
         }
+        // create a nav menu and append it to container
+        const nav = document.createElement('nav');
+        nav.className = "Application-nav";
+        const navItems = [
+            { title: 'Home', url: '#' },
+            { title: 'Projects', url: '#' },
+            { title: 'Games', url: '#' },
+            { title: 'Contact', url: '#' },
+        ];
+        navItems.forEach(item => {
+            const navItem = document.createElement('button');
+            navItem.textContent = item.title;
+            nav.appendChild(navItem);
+        });
+        container.appendChild(nav);
     };
 
     return <>
         <div className="Application-container">
-            <img className="Application-img" src="https://cdn.pixabay.com/photo/2024/05/24/19/06/bird-8785666_960_720.jpg"></img>
+            <img className="Application-img" src="https://cdn.pixabay.com/photo/2024/05/24/19/06/bird-8785666_960_720.jpg" alt='home '></img>
             <footer className="Application-footer" onClick={handleClick} >Appuyez pour commander</footer>
         </div>
     </>;
