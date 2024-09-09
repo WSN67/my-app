a271a78304c8537fe37bc490b359a397be948135
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-09-09 15:30:33 +0200

    updated code now it's "launching" a game

--- src/Game.jsx
+++ src/Game.jsx
@@ -5,14 +5,30 @@ function Game() {
 
     const launchGame = () => {
         let game = document.getElementsByTagName('select')[0].value;
-        if (game === "Default") {
-            return;
+
+
+        // TODO change to rendering a whole React element <TicTacToe />
+        switch (game) {
+            case "Morpion":
+                let div = document.createElement('div');
+                div.className = "TicTacToe";
+                document.getElementsByClassName("Game-home-container")[0].appendChild(div);
+                break;
+
+            default:
+                console.log("Game not found");
+                return;
         }
-        console.log("launch game " + game);
+
+
+
+        document.getElementsByTagName('select')[0].remove();
+        document.getElementsByTagName('button')[0].remove();
+
     }
 
     return <div className="Game-home-container">
-        <select name="Selectionner" id="select">
+        <select name="Selectionner" id="select" required={true} >
             <option value="Default">Sélectionnez un jeu</option>
             <option value="Morpion">Tic Tac Toe</option>
         </select>
