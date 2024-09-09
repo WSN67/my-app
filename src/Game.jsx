488a9624a1bb67f7655d901668efd7823435447a
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-09-09 16:32:01 +0200

    rendered game and added option for UX when no option selected

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,39 +1,42 @@
-import React from 'react';
-import "./Game.css"
+import React, { useState } from 'react';
+import "./Game.css";
+import TicTacToe from './TicTacToe';
 
 function Game() {
+    const [gameSelected, setGameSelected] = useState(false);
+    const [tictactoe, setTictactoe] = useState(false);
 
     const launchGame = () => {
-        let game = document.getElementsByTagName('select')[0].value;
+        let game = document.getElementById('select').value;
 
-
-        // TODO change to rendering a whole React element <TicTacToe />
         switch (game) {
-            case "Morpion":
-                let div = document.createElement('div');
-                div.className = "TicTacToe";
-                document.getElementsByClassName("Game-home-container")[0].appendChild(div);
+            case "TicTacToe":
+                setTictactoe(true);  // Show TicTacToe
+                setGameSelected(true);  // Hide select and button
                 break;
 
             default:
+                document.getElementById('error-message-button').style.display = "flex";
                 console.log("Game not found");
                 return;
         }
+    };
 
-
-
-        document.getElementsByTagName('select')[0].remove();
-        document.getElementsByTagName('button')[0].remove();
-
-    }
-
-    return <div className="Game-home-container">
-        <select name="Selectionner" id="select" required={true} >
-            <option value="Default">Sélectionnez un jeu</option>
-            <option value="Morpion">Tic Tac Toe</option>
-        </select>
-        <button className="playGame" onClick={launchGame}>Jouer</button>
-    </div>;
+    return (
+        <div className="Game-home-container">
+            {tictactoe && <TicTacToe />}
+            {!gameSelected && (  // Only show the select and button if no game is selected
+                <>
+                    <select id="select" required={true}>
+                        <option value="Default">Sélectionnez un jeu</option>
+                        <option value="TicTacToe">Tic Tac Toe</option>
+                    </select>
+                    <div id="error-message-button">Selectionnez un jeu dans la liste</div>
+                    <button className="playGame" onClick={launchGame}>Jouer</button>
+                </>
+            )}
+        </div>
+    );
 }
 
-export default Game
+export default Game;
---
+++ src/TicTacToe.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TicTacToe.css'
+
+function TicTacToe() {
+    return <div className="TicTacToe">
+        Tic Tac Toe
+    </div>;
+}
+
+export default TicTacToe
