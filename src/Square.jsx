d3b2d381754217c409ba354b378147642ad16ed7
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-09-10 14:06:12 +0200

    square works but game does not, still need a reset button

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,22 @@
-function Square(props) {
+import React, { useState } from 'react';
+import "./Square.css"
+
+function Square(prop) {
+
+    const [checked, setChecked] = useState("");
+
+    const handleClick = () => {
+        if (checked !== "") return;
+
+        if (prop.game === "TicTacToe") {
+            setChecked(prop.symbol);
+            prop.setSymbol({ game: "TicTacToe", value: prop.symbol === "X" ? "O" : "X" });
+        }
+    };
+
     return (
-        <div className="square" onClick={props.onClick}>
-            {props.value}
+        <div className="square" onClick={handleClick} >
+            {checked}
         </div>
     );
 }
--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TicTacToe.css'
+import Square from './Square';
 
 function TicTacToe() {
-    return <div className="TicTacToe">
-        Tic Tac Toe
+
+    const [symbol, setSymbol] = useState({ game: "TicTacToe", value: "X" });
+
+    // const checkwin = () => {
+    //     let s1 = document.getElementById("1");
+    //     console.log(s1);
+
+    // }
+
+
+
+
+
+    return <div className="TicTacToe" >
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="1" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="2" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="3" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="4" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="5" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="6" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="7" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="8" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="9" />
     </div>;
 }
 
