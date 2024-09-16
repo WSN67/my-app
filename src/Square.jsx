30781c9e02c791ccd8f0e7739fa97ce010e95abd
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-09-16 14:40:32 +0200

    TTT works but need to click one more time to end the game

--- src/Square.jsx
+++ src/Square.jsx
@@ -3,16 +3,62 @@ import "./Square.css"
 
 function Square(prop) {
 
+
     const [checked, setChecked] = useState("");
 
     const handleClick = () => {
-        if (checked !== "") return;
+        if (checked !== "" || prop.hasWin) return;
 
         if (prop.game === "TicTacToe") {
+            console.log("setSymbol");
+            let grid = document.getElementsByClassName("square");
+
+            checkWin(grid);
+            if (prop.hasWin) {
+                return;
+            }
+
             setChecked(prop.symbol);
+
+
+
             prop.setSymbol({ game: "TicTacToe", value: prop.symbol === "X" ? "O" : "X" });
         }
     };
+
+
+    const toggleWin = () => {
+        prop.setHasWin(!prop.hasWin);
+    }
+
+    const checkWin = (grid) => {
+        console.log("checkWin");
+
+        const win = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < win.length; i++) {
+            let [a, b, c] = win[i];
+            if (
+                grid[a].innerHTML !== "" &&
+                grid[a].innerHTML === grid[b].innerHTML &&
+                grid[a].innerHTML === grid[c].innerHTML
+            ) {
+                toggleWin();
+                alert(grid[a].innerHTML + " a gagne !");
+                return;
+            }
+        }
+
+    }
 
     return (
         <div className="square" onClick={handleClick} >
@@ -21,4 +67,4 @@ function Square(prop) {
     );
 }
 
-export default Square;
+export default Square
--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './TicTacToe.css'
 import Square from './Square';
 
@@ -6,26 +6,24 @@ function TicTacToe() {
 
     const [symbol, setSymbol] = useState({ game: "TicTacToe", value: "X" });
 
-    // const checkwin = () => {
-    //     let s1 = document.getElementById("1");
-    //     console.log(s1);
+    // const resetGrid = () => {
+    //     box = new Array(9).fill(null);
+    // };
 
-    // }
-
-
+    const [hasWin, setHasWin] = useState(false);
 
 
 
     return <div className="TicTacToe" >
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="1" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="2" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="3" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="4" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="5" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="6" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="7" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="8" />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} id="9" />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
     </div>;
 }
 
