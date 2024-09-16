09e1c2fbb16a8176020ca4f27c1ab63d3fd3ec48
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-09-16 14:55:21 +0200

    reset button not here + can't win if last pick

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -6,9 +6,11 @@ function TicTacToe() {
 
     const [symbol, setSymbol] = useState({ game: "TicTacToe", value: "X" });
 
-    // const resetGrid = () => {
-    //     box = new Array(9).fill(null);
-    // };
+    function appendResetButton() {
+        alert("yes");
+        console.log(document.getElementsByClassName("TicTacToe")[0]);
+    }
+
 
     const [hasWin, setHasWin] = useState(false);
 
@@ -23,7 +25,7 @@ function TicTacToe() {
         <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
         <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
         <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
-        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} />
+        <Square game={symbol.game} symbol={symbol.value} setSymbol={setSymbol} hasWin={hasWin} setHasWin={setHasWin} onload={appendResetButton} />
     </div>;
 }
 
