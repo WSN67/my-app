71723a1407e167d9db0232ea98138ebf506a690c
Nicolas NN <naouarniederst@etu.unistra.fr>
2024-09-28 14:43:04 +0200

    reupdate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 // self imports
 import Application from './Application';
-import Game from './Game';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
